bb880fe114116b1f2bb4a4fa4eee6bdabab39701
brunopazitto <brunopazitto@outlook.com>
2020-06-09 19:27:21 -0300

    atualização jobs2

--- src/components/JobsCard/index.js
+++ src/components/JobsCard/index.js
@@ -18,7 +18,7 @@ export default class jobsCard extends Component{
     }
 
     registerSocket(){
-        const socket = io('https://fullstackbackend2.herokuapp.com/');
+        const socket = io(process.env.REACT_APP_API_URL);
         //process.env.REACT_APP_API_URL
         socket.on('newjob', newjob=>{
             this.setState({jobs : [newjob,...this.state.jobs]});
